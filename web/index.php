70218b09971209887b9f04aaa7cd7f886e436994
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-07-07 14:33:56 +0200

    Devorigin is now the default application

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../hello/HelloKernel.php';
+require_once __DIR__.'/../devorigin/DevoriginKernel.php';
 
-$kernel = new HelloKernel('prod', false);
+$kernel = new DevoriginKernel('prod', false);
 $kernel->handle()->send();
