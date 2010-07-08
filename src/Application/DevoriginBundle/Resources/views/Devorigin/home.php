b93fbaed8b87b3fee347da476ba1248485116952
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-07-08 08:46:53 +0200

    Adding homepage content

--- src/Application/DevoriginBundle/Resources/views/Devorigin/home.php
+++ src/Application/DevoriginBundle/Resources/views/Devorigin/home.php
@@ -1,1 +1,34 @@
 <?php $view->extend('DevoriginBundle::layout') ?>
+
+<h1>Flexibilité et efficacité</h1>
+<p>Trouver le bon prestataire pour le bon projet n'est jamais chose facile. Il faut un prestataire <strong>techniquement solide</strong>, qui soit capable de <strong>comprendre</strong> et concrétiser les attentes des clients, tout en assurant un déploiement et un suivi efficace du produit.</p>
+<p class="em_text">Travailler avec <strong>DevOrigin</strong> vous permettra de bénéficier de la flexibilité d'un <strong>développeur Freelance</strong> capable de s'adapter à la spécificité de chaque projet. Mon expérience et l'utilisation d'un outil tel que <a href="http://www.symfony-project.org">symfony</a> vous permettront de mener à bien votre projet. N'hésitez pas à aller consulter la <a href="/realisations">liste des entreprises et collectivités m'ayant déjà fait confiance</a>.</p>
+
+<div class="margin_bottom_40"></div>
+
+<div class="section_320 fl margin_right_40">
+
+  <h1>Besoin d'un site ?</h1>
+  <p class="em_text">De la TPE au site de commerce en ligne, je développe <strong>tout type de site Internet</strong>. En fonction du budget de chacun, je peux facilement adapter la technologie employée. De l'utilisation d'un système de gestion de contenu déjà réalisé au développement sur mesure d'un outil de publication avec <a href="http://www.symfony-project.org">symfony</a>, toutes les options sont possibles.</p>
+  <p>N'hésitez pas à <a href="/contact">me contacter</a> ou à aller consulter quelques-une de mes réalisations :</p>
+
+  <ul class="list_01">
+    <li><a href="http://inscriptions.24heuresvelo.fr">Inscriptions des 24 heures vélo du Mans</a></li>
+    <li><a href="http://www.arnage.fr">Ville d'Arnage</a></li>
+    <li><a href="http://www.ville-rouillon.fr">Ville de Rouillon</a></li>
+    <li><a href="http://www.animezvous.com">Association AnimezVous</a></li>
+  </ul>
+  <p>
+    Toutes mes réalisations sont disponibles <a href="/realisations">ici</a>.
+  </p>
+
+</div>
+
+<div class="section_w250_without_padding fl">
+
+  <h1>Besoin d'un développeur ?</h1>
+
+  <p class="em_text">Issu d'une formation universitaire, je développe maintenant depuis plus de 10 ans.<br /> Je mets l'accent sur la qualité et la robustesse des développements réalisés, c'est pourquoi tous mes développements sont maintenant conduits par les tests.</p>
+  <p>Vous pouvez aller consulter mon <a href="/cv">CV</a> et <a href="/realisations">mes réalisations</a> pour plus de détails. Vous pouvez aussi <a href="/contact">me contacter</a> directement.</p>
+
+</div>
