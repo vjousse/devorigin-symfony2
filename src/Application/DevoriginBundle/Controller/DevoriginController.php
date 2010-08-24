2f7fa70a091ba3aa2e10b3d87f2a9fcd397c702e
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-08-24 12:00:06 +0200

    Fixed Devorigin Controller

--- src/Application/DevoriginBundle/Controller/DevoriginController.php
+++ src/Application/DevoriginBundle/Controller/DevoriginController.php
@@ -2,7 +2,7 @@
 
 namespace Application\DevoriginBundle\Controller;
 
-use Symfony\Framework\FoundationBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller;
 
 class DevoriginController extends Controller
 {
